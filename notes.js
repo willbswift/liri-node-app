7921268f1b2df3b08332c2f5fc70441ed3ae0bad
William B. Swift <willbswift@hotmail.com>
2018-10-12 19:15:00 -0500

    remembered to do an update!

---
+++ notes.js
@@ -0,0 +1,14 @@
+
+	// This creates an array with only the new data that you fed into node.
+let args = process.argv.slice(2);
+
+	// allows us to write data to other files
+let fs = require("fs");
+
+	// turns data into an array with commas marking out each item
+let things = data.toString().split(",");
+
+	//goes through an array and does the same thing to each item in the array
+.forEach( function( thing ) goes here )
+
+
